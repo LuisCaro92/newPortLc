a60a417d46eba475f16440d0a534068d74aa0ca0
luis caro <luis.carosalgado1@gmail.com>
2023-10-22 18:03:21 -0300

    con pocos avances

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 function App() {
   return (
     // Para poder generar un color degradante usar  bg-gradient-to-b from-slate-700
-    <div className="App">
+    <div className="App bg-image: url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.abc.es%2Fbienestar%2Fpsicologia-sexo%2Fpsicologia%2Fabci-personas-creativas-siete-habitos-para-desarrollar-mente-202112150049_noticia.html&psig=AOvVaw3_5G0VT0uWXKF7ntG2Insq&ust=1698094585135000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMD797bFioIDFQAAAAAdAAAAABAG')
+     w-full h-screen bg-cover">
       <Header/>
     <Home />
     </div>
