f656e707579a7b2daf80b3ba190846d4da3e226e
luis caro <luis.carosalgado1@gmail.com>
2023-11-01 18:30:21 -0300

    efecto backdroup creado y funcionando

--- src/componentes/About.js
+++ src/componentes/About.js
@@ -3,24 +3,24 @@ const About = () => {
     <div className="w-full h-screen bg-cover">
       <div className="justify-center ">
         <br></br>
-        <div className="mt-20 content-center text-center text-4xl">
+        <div className="mt-20  text-center text-2xl">
           <div className="flex font-bold">
-            <h1 className="text-8xl ml-10">DEVELOPER</h1>
-            <h1 className="mt-8 text-5xl">&</h1>
+            <h1 className="text-6xl ml-10">DEVELOPER</h1>
+            <h1 className="mt-8 text-3xl">&</h1>
           </div>
-          <h1 className="text-8xl text-center flex font-bold ml-10">
+          <h1 className="text-6xl text-center flex font-bold ml-10">
             DESIGNER UX
           </h1>
 
-          <p className="text-start p-10 ml-80 mr-30 mt-15">
+          <p className="text-start p-10 ml-80 mr-30 mt-15 font-semibold">
             I AM A DEVELOPER AND UX DESIGNER ESTABLISHED IN CHILE. MY LACK OF
             WORK EXPERIENCE IN THIS NEW WORLD IS COMPENSATED WITH MY CREATIVITY
             AND AUTONOMY IN CONSULTING LEARNING. I LOVE THE SOFT DESIGN AND THE
             TECHNOLOGY. I LOVE NATURE, PIZZA AND ART.
           </p>
 
-          <button class="ml-80 relative px-6 py-3 font-bold text-white group">
-            <span class="absolute inset-0 w-full h-full border-4 border-white hover:translate-y-10"></span>
+          <button class="ml-80 relative px-6 py-3 font-bold text-white group hover:translate-x-5 hover:shadow-xl hover:shadow-slate-300">
+            <span class="absolute inset-0 w-full h-full border-4 border-white "></span>
             <span class="relative">CONTACT ME</span>
           </button>
         </div>
--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,9 +1,9 @@
 const Header = () => {
   return (
-    <div className="shadow-2xl shadow-slate-300	w-full fixed top-0 left-0">
+    <div className="shadow-lg shadow-white backdrop-blur	w-full fixed top-0 left-0">
       <div className="sm:px-10 py-4 px-7 sm:flex justify-between items-center">
         <div className="flex text-3xl cursor-pointer items-center gap-2">
-        <span class="ml-10 relative px-10 py-3 font-bold text-white group">
+        <span class="ml-10 relative px-8 py-1 font-bold text-white group">
             <span class="absolute inset-0 w-full h-full opacity-40 transition duration-300 ease-out transform -translate-x-2 -translate-y-2 bg-gray-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
             <span class="absolute inset-0 w-full h-full border-4 border-white"></span>
             <span class="relative">LUIS CARO</span>
@@ -16,9 +16,9 @@ const Header = () => {
             className={`sm:flex pl-9 sm:pl-0 sm:items-center sm:pb-0 pb-12  sm:static sm:z-auto z-[-1] left-0 w-full transition-all duration-500 ease-in 
          `}
           >
-            <li className="my-7 sm:my-0 sm:ml-8">Contact</li>
-            <li className="my-7 sm:my-0 sm:ml-8">About</li>
-            <li className="my-7 sm:my-0 sm:ml-8">Project</li>
+            <li className="my-4 sm:my-0 sm:ml-8">Contact</li>
+            <li className="my-4 sm:my-0 sm:ml-8">About</li>
+            <li className="my-4 sm:my-0 sm:ml-8">Project</li>
           </ul>
         </div>
       </div>
